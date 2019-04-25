6ad0c4f42e0041c74bed88cfc3df326f56364cc3
Michel Bretschneider <embix@gmx.net>
2019-04-25 17:59:18 +0200

    +kubeless on play-with-k8s

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ some scripts to remember when playing with [k8s](https://labs.play-with-k8s.com/
 
 **Warning** Scripts use highly insecure constructs like `curl | sh` to keep them focussed on the topic - don't ever use them outside a playground!
 
-i.e. to set up a kubernetes master node
+i.e. to set up kubeless in labs.play-with-k8s:
 
     curl https://raw.githubusercontent.com/embix/playwith/master/kubernetes/init-master-with-network.sh | bash
---
+++ kubernetes/init-k8s-with-kubeless.sh
@@ -0,0 +1,26 @@
+# almost any shell
+
+# set up k8s
+kubeadm init --apiserver-advertise-address $(hostname -i)
+kubectl apply -n kube-system -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 |tr -d '\n')"
+
+# set up kubeless
+export RELEASE=$(curl -s https://api.github.com/repos/kubeless/kubeless/releases/latest | grep tag_name | cut -d '"' -f 4)
+kubectl create ns kubeless
+kubectl create -f https://github.com/kubeless/kubeless/releases/download/$RELEASE/kubeless-$RELEASE.yaml
+
+# check everything is as it should be
+kubectl get pods -n kubeless
+kubectl get deployment -n kubeless
+kubectl get customresourcedefinition
+
+# install kubeless cli so we can play righ away
+export OS=$(uname -s| tr '[:upper:]' '[:lower:]')
+curl -OL https://github.com/kubeless/kubeless/releases/download/$RELEASE/kubeless_$OS-amd64.zip
+# basically `unzip kubeless_$OS-amd64.zip` but play with k8s has neither unzip nor apk
+# so we have to improvise...
+docker run -it --rm -v $PWD:/halp ubuntu:latest unzip /halp/kubeless_$OS-amd64.zip
+mv bundles/kubeless_$OS-amd64/kubeless /usr/local/bin/
+
+# final check
+which kubeless
